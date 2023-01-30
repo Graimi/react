f650c52e88e1bb22bb8b110058f04e5f71367128
Graimi <113901525+Graimi@users.noreply.github.com>
2023-01-30 12:41:46 +0100

    20-react-hook-form-3 class done

--- 06-forms/src/components/Form.jsx
+++ 06-forms/src/components/Form.jsx
@@ -2,12 +2,18 @@ import React from 'react';
 import { useForm } from 'react-hook-form';
 
 const Form = () => {
-  const { handleSubmit, register } = useForm({
+  const { handleSubmit, register, formState, watch } = useForm({
     defaultValues: {
       superhero: '',
       superpower: '',
+      country: '',
+      terms: false,
     },
   });
+
+  const country = watch('country');
+
+  console.log(country);
 
   const onSubmit = (values) => {
     console.log(values);
@@ -18,7 +24,25 @@ const Form = () => {
       <h1>Eiii</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor="superhero">Nombre del superhéroe:</label>
-        <input {...register('superhero')} type="text" id="superhero" />
+        <input
+          {...register('superhero', {
+            required: {
+              value: true,
+              message:
+                'Necesitas un nombre de superhéroe para poder continuar ⛔',
+            },
+            minLength: {
+              value: 2,
+              message:
+                'El tamaño del nombre tiene que tener mínimo 2 caracteres',
+            },
+          })}
+          type="text"
+          id="superhero"
+        />
+        {formState.errors.superhero ? (
+          <p>{formState.errors.superhero.message} </p>
+        ) : null}
 
         <br />
 
@@ -27,7 +51,40 @@ const Form = () => {
 
         <br />
 
-        <button type="submit">Enviar!</button>
+        <label htmlFor="country">¿De qué país eres?</label>
+        <select {...register('country')} name="country" id="country">
+          <option value="">-</option>
+          <option value="spain">Spain</option>
+          <option value="other">Other</option>
+        </select>
+
+        <br />
+
+        {country === 'spain' ? (
+          <>
+            <input
+              {...register('terms', {
+                required: {
+                  value: true,
+                  message: 'Para continuar tiene que aceptar las condiciones',
+                },
+              })}
+              type="checkbox"
+              id="terms"
+            />
+            <label htmlFor="term">Acepto los términos y condiciones</label>
+
+            <br />
+
+            {formState.errors.terms ? formState.errors.terms.message : null}
+          </>
+        ) : null}
+
+        <br />
+
+        <button type="submit" disabled={!formState.isDirty}>
+          Enviar!
+        </button>
       </form>
     </div>
   );
