643672c8d1db18f5bdd942ee288b92e9ac4ea3b4
Graimi <113901525+Graimi@users.noreply.github.com>
2023-01-31 10:27:13 +0100

    22-router-part-1 class done (amazing btw)

---
+++ 07-react-router/src/App.jsx
@@ -0,0 +1,26 @@
+import { NavLink, Outlet } from "react-router-dom";
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <header>
+        <h1>React Router v6 🫡</h1>
+      </header>
+      <div>
+        <nav>
+          <NavLink to="">Home 🏠</NavLink>
+          <NavLink to="heroes">Heroes 🦸</NavLink>
+          <NavLink to="about">About 👨‍💻</NavLink>
+        </nav>
+      </div>
+      <main>
+        <Outlet />
+      </main>
+      <footer>Created by RockTheCode with 💖</footer>
+    </div>
+  )
+}
+
+export default App
